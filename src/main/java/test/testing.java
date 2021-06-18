d0a9ee2ff1e7eafe280601efa65d9c3d4d280aaa
vistasunil <37858762+vistasunil@users.noreply.github.com>
2021-06-18 23:40:10 +0530

    Update testing.java

--- src/main/java/test/testing.java
+++ src/main/java/test/testing.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class testing{
    public static void main(String[] args) {
-       System.setProperty("webdriver.chrome.driver","/usr/local/bin/chromedriver");	// <-- Change this path
+       System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");	// <-- Change this path
        WebDriver driver = new ChromeDriver();
        String baseUrl = "http://104.154.106.116:82/devopsIQ/";
        String expectedTitle = "Jenkins Webhook Website";
