52c29e2230491e07e2e0394c2abb097424af5aee
vistasunil <37858762+vistasunil@users.noreply.github.com>
2021-06-18 23:36:37 +0530

    Update and rename Testing.java to testing.java

--- src/main/java/test/testing.java
+++ src/main/java/test/testing.java
@@ -3,7 +3,7 @@ package test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Testing{
+public class testing{
    public static void main(String[] args) {
        System.setProperty("webdriver.chrome.driver","/usr/local/bin/chromedriver");	// <-- Change this path
        WebDriver driver = new ChromeDriver();
