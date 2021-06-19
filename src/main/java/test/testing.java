4645f077e5418bd6ef5bc47b9ddd3edb0d576dea
vistasunil <37858762+vistasunil@users.noreply.github.com>
2021-06-19 19:10:39 +0530

    Update testing.java

--- src/main/java/test/testing.java
+++ src/main/java/test/testing.java
@@ -7,8 +7,8 @@ public class testing{
    public static void main(String[] args) {
        System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");	// <-- Change this path
        WebDriver driver = new ChromeDriver();
-       String baseUrl = "https://www.thecloudtrain.com";
-       String expectedTitle = "DevOps Workshop | Cloudtrain";
+       String baseUrl = "http://localhost:82/devopsIQ";
+       String expectedTitle = "Jenkins Webhook Website";
        String actualTitle = "";
        driver.get(baseUrl);
        actualTitle = driver.getTitle();
