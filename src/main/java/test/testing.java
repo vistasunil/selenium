7200e0d22c074bcf11dbbfeccb15ba22f9fde26c
vistasunil <37858762+vistasunil@users.noreply.github.com>
2021-06-21 00:00:02 +0530

    Update testing.java

--- src/main/java/test/testing.java
+++ src/main/java/test/testing.java
@@ -2,13 +2,18 @@ package test;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class testing{
    public static void main(String[] args) {
-       System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");	// <-- Change this path
-       WebDriver driver = new ChromeDriver();
-       String baseUrl = "http://localhost:82/devopsIQ";
-       String expectedTitle = "Jenkins Webhook Website";
+       System.setProperty("webdriver.chrome.driver","/usr/local/bin/chromedriver");	// <-- Change this path
+       ChromeOptions options = new ChromeOptions();        
+        //add the headless argument
+       options.addArguments("headless");        
+        //pass the options parameter in the Chrome driver declaration
+       WebDriver driver = new ChromeDriver(options);        
+       String baseUrl = "https://www.thecloudtrain.com";
+       String expectedTitle = "DevOps Workshop | Cloudtrain";
        String actualTitle = "";
        driver.get(baseUrl);
        actualTitle = driver.getTitle();
