b4f43ed5b207beed52cb7ce9ee9eb34a5dd86119
vistasunil <37858762+vistasunil@users.noreply.github.com>
2021-07-25 23:55:31 +0530

    Update testing.java

--- src/main/java/test/testing.java
+++ src/main/java/test/testing.java
@@ -10,9 +10,6 @@ public class testing{
        ChromeOptions options = new ChromeOptions();        
         //add the headless argument
        //options.addArguments("--headless", "--disable-gpu", "--dump-dom");    
-      options.addArguments("--user-data-dir=/home/ubuntu"); 
-options.addArguments("--disable-infobars"); 
-options.addArguments("--lang=en"); 
        options.addArguments("--headless", "--dump-dom", "--disable-gpu", "--window-size=1920,1200","--ignore-certificate-errors","--disable-extensions","--no-sandbox","--disable-dev-shm-usage");
        WebDriver driver = new ChromeDriver(options);        
        String baseUrl = "https://thecloudtrain.com/";
