beb231048315f8dcdd580f32e937fd1e8083b9b4
vistasunil <37858762+vistasunil@users.noreply.github.com>
2021-07-25 23:04:44 +0530

    Update testing.java

--- src/main/java/test/testing.java
+++ src/main/java/test/testing.java
@@ -9,17 +9,7 @@ public class testing{
        System.setProperty("webdriver.chrome.driver","/usr/local/bin/chromedriver");	// <-- Change this path
        ChromeOptions options = new ChromeOptions();        
         //add the headless argument
-       //options.addArguments("--headless", "--disable-gpu", "--dump-dom");        
-       options.addArguments("disable-infobars");
-       //options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
-       options.setExperimentalOption("useAutomationExtension", false);
-
-        options.addArguments("--disable-gpu");
-        options.addArguments("--disable-extensions");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--headless");
-        options.addArguments("--window-size=1580,1280");
+       options.addArguments("--headless") //, "--disable-gpu", "--dump-dom");        
        //options.addArguments("--headless", "window-size=1024,768", "--no-sandbox"); //("--headless", "--disable-gpu", "--window-size=1920,1200","--ignore-certificate-errors","--disable-extensions","--no-sandbox","--disable-dev-shm-usage");
        WebDriver driver = new ChromeDriver(options);        
        String baseUrl = "https://www.thecloudtrain.com/";
