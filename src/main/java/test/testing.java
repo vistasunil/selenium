a8488f3e0b1af3af0a3e10d1a8bce5fdca42efc7
vistasunil <37858762+vistasunil@users.noreply.github.com>
2021-07-25 23:44:56 +0530

    Update testing.java

--- src/main/java/test/testing.java
+++ src/main/java/test/testing.java
@@ -9,7 +9,10 @@ public class testing{
        System.setProperty("webdriver.chrome.driver","/usr/local/bin/chromedriver");	// <-- Change this path
        ChromeOptions options = new ChromeOptions();        
         //add the headless argument
-       //options.addArguments("--headless", "--disable-gpu", "--dump-dom");        
+       //options.addArguments("--headless", "--disable-gpu", "--dump-dom");    
+      options.add_argument("--user-data-dir=/home/ubuntu") #Path to your chrome user-data/profile
+options.add_argument('--disable-infobars') #disable the automation prompt bar
+options.add_argument('--lang=en') #Set language to English
        options.addArguments("--headless", "--dump-dom", "--disable-gpu", "--window-size=1920,1200","--ignore-certificate-errors","--disable-extensions","--no-sandbox","--disable-dev-shm-usage");
        WebDriver driver = new ChromeDriver(options);        
        String baseUrl = "https://thecloudtrain.com/";
