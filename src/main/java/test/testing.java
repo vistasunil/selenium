9edfbdf71999306094b607a8001d29a38a243c07
vistasunil <37858762+vistasunil@users.noreply.github.com>
2021-07-26 16:56:14 +0530

    Update testing.java

--- src/main/java/test/testing.java
+++ src/main/java/test/testing.java
@@ -6,15 +6,15 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 public class testing{
    public static void main(String[] args) {
-       
+       System.setProperty("webdriver.chrome.driver","/usr/local/bin/chromedriver");
        ChromeOptions options = new ChromeOptions();        
         //add the headless argument
-       options.setBinary("/opt/google/chrome/chrome");
+       options.setBinary("/opt/google/chrome/chrome");   
        options.addArguments("window-size=1400,800");  
        options.addArguments("headless");  
        //options.addArguments("--headless", "--dump-dom", "--disable-gpu", "--window-size=1920,1200","--ignore-certificate-errors","--disable-extensions","--no-sandbox","--disable-dev-shm-usage");
-       System.setProperty("webdriver.chrome.driver","/usr/local/bin/chromedriver");	// <-- Change this path
-      WebDriver driver = new ChromeDriver(options);        
+      	// <-- Change this path
+       WebDriver driver = new ChromeDriver(options);        
        String baseUrl = "https://thecloudtrain.com/";
        String expectedTitle = "Jenkins Webhook Website";
        String actualTitle = "";
