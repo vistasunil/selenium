c46fa8d39a88054fbf402c2be1ebad34affd7574
vistasunil <37858762+vistasunil@users.noreply.github.com>
2021-07-26 18:05:06 +0530

    Update sel.py

--- sel.py
+++ sel.py
@@ -1,14 +1,23 @@
-import os
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
+import os  
+from selenium import webdriver  
+from selenium.webdriver.common.keys import Keys  
+from selenium.webdriver.chrome.options import Options  
 
-chrome_options = Options()
-chrome_options.add_argument("--headless")
+chrome_options = Options()  
+chrome_options.add_argument("--headless")  
 chrome_options.binary_location = '/usr/bin/google-chrome'
 
-driver = webdriver.Chrome(executable_path=os.path.abspath("/usr/local/bin/chromedriver"), chrome_options=chrome_options)
-driver.get("http://www.duo.com")
+driver = webdriver.Chrome(executable_path=os.path.abspath("/usr/local/bin/chromedriver"), chrome_options=chrome_options)  
+driver.get("https://thecloudtrain.com");
+expectedTitle = "DevOps Workshop | Cloudtrain";
 actualTitle = driver.title;
-print(actualTitle)
+print(actualTitle);
+if actualTitle == expectedTitle:
+    print("Website Title Expected: "+ expectedTitle);
+    print("Website Title Actual: "+ actualTitle);
+    print("TEST PASSED!");
+else:
+    print("Website Title Expected: "+ expectedTitle);
+    print("Website Title Actual: "+ actualTitle);
+    print("TEST FAILED!");
 driver.close()
