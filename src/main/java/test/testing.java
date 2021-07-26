2aaec7849c48acdda991d4f193460cad1ff69064
vistasunil <37858762+vistasunil@users.noreply.github.com>
2021-07-26 14:07:39 +0530

    Update testing.java

--- src/main/java/test/testing.java
+++ src/main/java/test/testing.java
@@ -12,8 +12,6 @@ public class testing{
        options.setBinary("/opt/google/chrome/chrome");
        options.addArguments("window-size=1400,800");  
        options.addArguments("headless");  
-      options.addArguments("start-maximized");
-options.setExperimentalOption("useAutomationExtension", false);
        //options.addArguments("--headless", "--dump-dom", "--disable-gpu", "--window-size=1920,1200","--ignore-certificate-errors","--disable-extensions","--no-sandbox","--disable-dev-shm-usage");
        System.setProperty("webdriver.chrome.driver","/usr/local/bin/chromedriver");	// <-- Change this path
       WebDriver driver = new ChromeDriver(options);        
