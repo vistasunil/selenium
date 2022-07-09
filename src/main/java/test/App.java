77cf3b7b0a78e1244c7d59d0b2235b5056c06bba
vistasunil <37858762+vistasunil@users.noreply.github.com>
2022-07-09 11:38:27 +0530

    Update App.java

--- src/main/java/test/App.java
+++ src/main/java/test/App.java
@@ -1,48 +1,51 @@
 package test;
-import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import java.util.concurrent.TimeUnit;
 
 public class App {
+
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
-		ChromeOptions options = new ChromeOptions();        
-        //add the headless argument
-        options.addArguments("headless");
-		WebDriver driver = new ChromeDriver(options);
+		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.ibm.com");
-		driver.manage().window().maximize();		
+		driver.manage().window().maximize();
+
 		driver.findElement(By.id("q")).sendKeys("DevOps");
 		driver.findElement(By.className("ibm-search-link")).click();
-		driver.manage().timeouts().implicitlyWait(3,TimeUnit.SECONDS);		
-		
+		driver.manage().timeouts().implicitlyWait(8,TimeUnit.SECONDS);
+
 		String firstData = driver.findElement(By.className("ibm--result-item__title")).getText();
 		String CheckData = "DevOps – IBM Developer – IBM Developer";
 		System.out.println(firstData);
+		
 		if(firstData.equals(CheckData)) {
 			driver.findElement(By.className("ibm--result-item__title")).click();
-			driver.manage().timeouts().implicitlyWait(3,TimeUnit.SECONDS);
+			driver.manage().timeouts().implicitlyWait(8,TimeUnit.SECONDS);
 		}
 		else {
 			System.out.println("Item not found");
 		}
 		
-		String actualData = driver.findElement(By.className("developer--card__title")).getText();
-		String expectedData = "Use Tekton and OpenShift operators to automate application deployment";
+		String actualData = driver.findElement(By.className("developerCardTitle")).getText();
+		String expectedData = "What is DevSecOps and why is it so important?";
 		System.out.println(actualData);
 		if(actualData.equals(expectedData)) {
 			System.out.println("Found String!\n---------------");			
-			String dateView = driver.findElement(By.className("ibmcode_upcomingevents")).getText();
-			String timeView = driver.findElement(By.className("developer--card__location")).getText();
-			System.out.println("Next Event is on: \n"+ dateView + "\n"+ timeView);
+			String solutions = driver.findElement(By.className("bx--accordion--md")).getText();
+			String trending = driver.findElement(By.id("trending__panel")).getText();
+			System.out.println("Featured solutions: \n"+ solutions + "\n Trending:\n---------------\n" + "\n"+ trending);
 			System.out.println("---------------\nTest Successful");
-			driver.close();	
+			//driver.close();	
 		}
 		else {
 			System.out.println("Oops!! String Not Found.");
 		}
-	
+
+
 	}
+
 }
