1a296cdcbc646f5fbc505f07943efc36faa4dc4c
vistasunil <37858762+vistasunil@users.noreply.github.com>
2023-06-18 11:54:05 +0200

    Update sel.py

--- sel.py
+++ sel.py
@@ -2,16 +2,18 @@ import os
 from selenium import webdriver  
 from selenium.webdriver.common.keys import Keys  
 from selenium.webdriver.chrome.options import Options  
+from selenium.webdriver.chrome.service import Service
 
 chrome_options = Options()  
 chrome_options.add_argument("--headless")  
-chrome_options.binary_location = '/usr/bin/google-chrome'
 
-driver = webdriver.Chrome(executable_path=os.path.abspath("/usr/local/bin/chromedriver"), chrome_options=chrome_options)  
+service = Service(executable_path="/usr/local/bin/chromedriver")
+driver = webdriver.Chrome(service=service, options=chrome_options)  
 driver.get("https://thecloudtrain.com");
 expectedTitle = "DevOps Workshop | Cloudtrain";
 actualTitle = driver.title;
 print(actualTitle);
+
 if actualTitle == expectedTitle:
     print("Website Title Expected: "+ expectedTitle);
     print("Website Title Actual: "+ actualTitle);
@@ -20,4 +22,5 @@ else:
     print("Website Title Expected: "+ expectedTitle);
     print("Website Title Actual: "+ actualTitle);
     print("TEST FAILED!");
+
 driver.close()
